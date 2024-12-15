0a45ebf42e986ae6d549e847c79c00c0a0dd69cf
zjulzy <lenmainlzy@gmail.com>
2024-12-15 16:57:59 +0800

    其他电脑拉取开发尝试，需要解决playable visualizer包导入问题

--- Assets/Scripts/World/EnemySpawner.cs
+++ Assets/Scripts/World/EnemySpawner.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private GameObject _enemyPrefab; // The enemy prefab to spawn
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
