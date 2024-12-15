31df6862311b9d343d5c8a0906b8961f9494fe9e
zju_lzy <22121026@zju.edu.cn>
2024-12-15 09:47:56 +0800

    导入node canvas示例代码

--- Assets/Scripts/AI/MoveToCharacter.cs
+++ Assets/Scripts/AI/MoveToCharacter.cs
@@ -10,6 +10,7 @@ namespace NodeCanvas.Tasks.Actions {
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
+			
 			return null;
 		}
 
