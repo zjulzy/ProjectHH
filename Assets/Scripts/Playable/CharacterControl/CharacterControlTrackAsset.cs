a609f081041355f0c908b063448cdaf119d1fcc7
zju_lzy <22121026@zju.edu.cn>
2025-04-19 23:58:56 +0800

    删除老playable track

--- Assets/Scripts/Playable/CharacterControl/CharacterControlTrackAsset.cs
+++
@@ -1,8 +0,0 @@
-using ProjectHH;
-using UnityEngine;
-using UnityEngine.Timeline;
-
-[TrackClipType(typeof(BlockMovePlayableAsset)), TrackClipType(typeof(CheckContinuePlayableAsset)), TrackClipType(typeof(MoveInterruptPlayableAsset))]
-public class CharacterControlTrackAsset : TrackAsset
-{
-}
